e39e006ed14445739d0d67d424fc4d42c2b0777e
Shreyasg13 <shreyasgondane@gmail.com>
2023-09-24 23:41:51 -0400

    adding tests for the project

---
+++ tests/test_assistant.py
@@ -0,0 +1,10 @@
+import pytest
+from app import assistant  # Assuming the assistant module is inside an app directory
+
+# Testing the stop keyword functionality in assistant
+def test_stop_keyword():
+    # Replace with your actual implementation details
+    response = assistant.respond("stop")
+    assert response == "Stopping the assistant."  # Expected response when saying "stop"
+
+# Add more tests as needed
